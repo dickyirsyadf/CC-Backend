0f70262b2babf5e61a55fabf28a1c95b6a400579
dickyirsyadf <dickyirsyadf@gmail.com>
2023-12-17 13:05:23 +0700

    DB auth changes, qr not tested

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,9 +3,10 @@ const prisma = require("../config/db");
 
 const register = async (req, res) => {
   // harusnya bisa nangkap role tapi gua bingung caranya gimana
+  // role dikasih haidar pas klik register biar langsung input ke tabel user, 
+  // jadi kita gausah pusing nyari role, klik auth juga auto masuk sih.
   // const register = async (req, res, role) => {
-
-  const { userId, roleId } = req.body;
+  const { userId } = req.body;
 
   try {
     // * Cek apakah pengguna dengan UUID tersebut sudah terdaftar
@@ -31,7 +32,7 @@ const register = async (req, res) => {
     const createdUser = await prisma.user.create({
       data: {
         userId,
-        roleId,
+        role,
         // roleId: roleInfo.roleId,
       },
     });
@@ -49,7 +50,7 @@ const login = async (req, res, role) => {
   const { userId } = req.body;
 
   try {
-    const entity = role === "user" ? "user" : "organization";
+    const entity = role === "User" ? "User" : "Organization";
 
     // Cari pengguna di PostgreSQL berdasarkan UUID
     const result = await prisma[entity].findUnique({
--- src/controllers/qrcode.controller.js
+++ src/controllers/qrcode.controller.js
@@ -1,32 +1,65 @@
 const prisma = require("../config/db");
-const QRCode = require("qrcode");
+const QRCode = require('qrcode');
+const crypto = require('crypto');
+const fs = require('fs/promises');
+const prisma = new PrismaClient();
 
-const generateQRCode = async (eventId) => {
+// Function to generate a unique hash based on the current date
+function generateUniqueHash() {
+  const today = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
+  const hash = crypto.createHash('sha256').update(today).digest('hex'); // Hash the date
+  return hash;
+}
+
+// Function to generate QR code with a specific size
+async function generateQRCode(data, size) {
   try {
-    const today = new Date();
-    // const token = `${today.getFullYear()}${
-    //   today.getMonth() + 1
-    // }${today.getDate()}`;
+    // Generate QR code image from data
+    const qrCodeImage = await QRCode.toFile(`qr_${data}.png`, data, {
+      width: size,
+      height: size,
+    });
 
-    // Simpan QRHash ke database
-    const qrHash = await prisma.qRHash.create({
+    console.log(`QR Code generated: qr_${data}.png`);
+    return `qr_${data}.png`;
+  } catch (error) {
+    console.error('Error generating QR code:', error);
+    throw error;
+  }
+}
+
+// Function to save QR code data to the database
+async function saveQRCodeToDatabase(hash, imageUrl) {
+  try {
+    const savedQRCode = await prisma.qRCode.create({
       data: {
-        eventId: parseInt(eventId),
-        date: today,
+        hash,
+        imageUrl,
       },
     });
 
-    // Buat QR Code
-    const qrCodeData = JSON.stringify({ qrHashId: qrHash.qrHashId });
-    const qrCode = await QRCode.toDataURL(qrCodeData);
-
-    return qrCode;
+    console.log('QR Code data saved to database:', savedQRCode);
   } catch (error) {
-    console.error(error);
-    throw new Error("Internal server error");
+    console.error('Error saving QR code data to database:', error);
+    throw error;
   }
-};
+}
 
+// Generate a unique hash for today's date
+const uniqueHash = generateUniqueHash();
+
+// Generate QR code with the unique hash and size 512x512
+generateQRCode(uniqueHash, 512)
+  .then(async (imagePath) => {
+    // Save QR code image path to the database using Prisma
+    await saveQRCodeToDatabase(uniqueHash, imagePath);
+  })
+  .catch(async (error) => {
+    console.error('Error generating and saving QR code:', error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect(); // Close the database connection
+  });
 module.exports = {
   generateQRCode,
 };
